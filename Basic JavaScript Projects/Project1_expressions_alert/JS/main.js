cd1f75c6498b787fcc6ec885b4ef43a18bdd4a9a
AhmedDhel <ahmeddheel@gmail.com>
2026-02-04 03:16:47 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,5 +3,16 @@
 var A = "This is a string";
 document.writeln(A);
 
+document.writeln("<br>");
+
 var B = "Alerting a string";
 window.alert(B);
+
+var C = "Ahmed said, \"I am a JavaScript developer, don\'t you know that?\"";
+document.writeln(C);
+
+document.writeln("<br>");
+
+var D = "It\'s a beautiful day!"
++ " Isn\'t it?";
+document.writeln(D);
